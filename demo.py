45eda884755267236abacea90d6d4a9c2fccb94e
UO289256 <146960342+UO289256@users.noreply.github.com>
2024-09-10 16:43:20 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -158,9 +158,15 @@ if not st.session_state.resumen_generado:
         st.write(
             "Los Modelos de Lenguaje de Gran Escala (LLMs) son sistemas avanzados entrenados con grandes cantidades de datos textuales..."
         )
+        st.write(
+            "Los Modelos de Lenguaje de Gran Escala (LLMs) son sistemas avanzados entrenados con grandes cantidades de datos textuales. Esto les permite comprender y generar lenguaje natural, así como identificar patrones complejos y generar respuestas coherentes y contextualmente apropiadas.\n\n"
+            "Debido a su habilidad para abordar una amplia variedad de tareas y comprender múltiples idiomas, estos modelos están revolucionando la interacción entre humanos y máquinas, proporcionando soluciones más precisas y efectivas en diversas áreas."
+        )
         st.header('Enfoque RAG')
         st.write(
-            "Los LLMs son sistemas avanzados entrenados con grandes volúmenes de datos textuales. Sin embargo, estos modelos no están específicamente entrenados con datos particulares de usuarios individuales..."
+            "Los LLMs son sistemas avanzados entrenados con grandes volúmenes de datos textuales. Sin embargo, estos modelos no están específicamente entrenados con datos particulares de usuarios individuales.\n\n"
+            "La Generación Aumentada por Recuperación (RAG) aborda esta limitación al integrar datos específicos con la información general a la que los LLMs ya tienen acceso.\n\n"
+            "En el enfoque RAG, los datos se cargan y preparan para consultas mediante un proceso de 'indexación' . Las consultas de los usuarios se ejecutan sobre este índice, que filtra la información para identificar el contexto más relevante. Este contexto, junto con la consulta, se envía al LLM junto con un mensaje, permitiendo al modelo proporcionar una respuesta precisa y adecuada."
         )
 
     # Generador de resúmenes
@@ -175,16 +181,19 @@ if not st.session_state.resumen_generado:
     with col2:
         enfoque = st.selectbox('Selecciona el enfoque con el que quieres obtener el resumen.', ['LLM Tradicional', 'Enfoque RAG'])
 
-    st.write(
-        "RECUERDA: El enfoque tradicional emplea un modelo de aprendizaje profundo para generar los resúmenes..."
-    )
+        st.write(
+            "RECUERDA: El enfoque tradicional emplea un modelo de aprendizaje profundo para generar los resúmenes. Sin embargo, un BOE estándar a menudo supera la capacidad del modelo, lo que puede resultar en resúmenes insatisfactorios y en posibles inconsistencias."
+        )
+
+    
 
     if enfoque == 'LLM Tradicional':
         summary_method = 'LLM Tradicional'
     else:
         summary_method = st.selectbox('Selecciona el método de resumen', ['Document Summary Index', 'Summary Index'])
         st.write(
-            "Un Index es una estructura de datos organizada en nodos a partir de los cuales se obtiene el resumen del documento..."
+            "Un Index es una estructura de datos organizada en nodos a partir de los cuales se obtiene el resumen del documento.\n\n"
+            "A continuación, se puede optar entre dos métodos distintos para estructurar los datos, lo que resultará en diferentes tipos de resúmenes de los BOEs."
         )
 
     if st.button('Generar Resumen'):
